9d4e3595ac711f182d1479c94e9b4807dfdcac63
MariusNV <MariusNVaduva@Gmail.Com>
2012-11-27 18:24:12 +0100

    commit

---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+@SuppressLint("SetJavaScriptEnabled") public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
